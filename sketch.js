012cd1caa87f4d0eb490e8d8c9263573f61f3665
Nathan <68413475+NatG91@users.noreply.github.com>
2020-08-23 10:56:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,20 +23,19 @@ bobDiameter=40
 
 	//Create the Bodies Here.
 
-roofObject=new Roof(220,290,200,20);
+roofObject=new Roof(800,400,200,20);
 
-bobObject1=new Ball(720,720,bobDiameter);
-bobObject2=new Ball(760,760,bobDiameter);
-bobObject3=new Ball(800,800,bobDiameter);
-bobObject4=new Ball(840,840,bobDiameter);
-bobObject5=new Ball(880,880,bobDiameter);
+bobObject1=new Ball(720,600,bobDiameter);
+bobObject2=new Ball(760,600,bobDiameter);
+bobObject3=new Ball(800,600,bobDiameter);
+bobObject4=new Ball(840,600,bobDiameter);
+bobObject5=new Ball(880,600,bobDiameter);
 
-rope1=new Rope(bobObject1.body,roofObject.body,bobDiameter*2,0);
-rope2=new Rope(bobObject2.body,roofObject.body,bobDiameter*2,0);
-rope3=new Rope(bobObject3.body,roofObject.body,bobDiameter*2,0);
-rope4=new Rope(bobObject4.body,roofObject.body,bobDiameter*2,0);
-rope5=new Rope(bobObject5.body,roofObject.body,bobDiameter*2,0);
-
+rope1=new Rope(bobObject1.body,roofObject.body,-80,0);
+rope2=new Rope(bobObject2.body,roofObject.body,-40,0); 
+rope3=new Rope(bobObject3.body,roofObject.body,0,0); 
+rope4=new Rope(bobObject4.body,roofObject.body,40,0); 
+rope5=new Rope(bobObject5.body,roofObject.body,80,0);
 
 
 
@@ -55,23 +54,23 @@ function draw() {
 
   roofObject.display();
 
-  bobObject1.display();
-  bobObject2.display();
-  bobObject3.display();
-  bobObject4.display();
-  bobObject5.display();
-
   rope1.display();
   rope2.display();
   rope3.display();
   rope4.display();
   rope5.display();
 
+bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+
 }
 
 function keyPressed(){
   if (keyCode === UP_ARROW){
-      Matter.Body.applyForce(bobObject4.body,bobObject4.body.position,{x:85,y:-85});
+      Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-50});
   }
 }
 
